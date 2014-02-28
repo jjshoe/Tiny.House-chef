8c1646bf13a9ec4e76b23e8182cc92a87c383299
jjshoe <jjshoe@gmail.com>
2014-02-27 23:42:50 -0600

    Just pull the master branch, we don't need a deploy. As well as
    install all our gems, and run our migrations please.

--- recipes/default.rb
+++ recipes/default.rb
@@ -11,10 +11,27 @@ rbenv_gem 'rails' do
   action :install
 end
 
-git "/home/vagrant/Tiny.House" do
-  repository "https://github.com/jjshoe/Tiny.House.git"
-  reference "master"
+git '/home/vagrant/Tiny.House' do
+  repository 'https://github.com/jjshoe/Tiny.House.git'
+  reference 'master'
+  enable_checkout false
   action :sync
-  user "vagrant"
-  group "vagrant"
+  user 'vagrant'
+  group 'vagrant'
+end
+
+rbenv_script 'install_gems' do
+  rbenv_version '2.1.0'
+  user          'root'
+  group         'root'
+  cwd           '/home/vagrant/Tiny.House'
+  code          %{bundle update}
+end
+
+rbenv_script 'migrate_rails_database' do
+  rbenv_version '2.1.0'
+  user          'vagrant'
+  group         'vagrant'
+  cwd           '/home/vagrant/Tiny.House'
+  code          %{rake RAILS_ENV=development db:migrate}
 end
